5aff6b28727566523c6f7e2957a6416df364efca
Bahar Tehranipoor <bahartehranipoor@Bahars-MBP.lan>
2023-10-04 11:06:48 -0400

    Setting up authentication for py script

---
+++ m-script.py
@@ -0,0 +1,22 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy import util
+
+client_id = "CLIENT ID"
+client_secret = "CLIENT SECRET"
+username = "USERNAME"
+
+scope = "user-library-read"
+redirect_uri = "CALLBACK"
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+if token: 
+    sp = spotipy.Spotify(auth=token)
+else:
+    print("Can't get token for", username)
+
+
+
