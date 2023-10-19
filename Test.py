bddf80fa9973d5395075da181db6fcda4636bb9d
Bahar Tehranipoor <bahartehranipoor@Bahars-MBP.lan>
2023-10-19 14:54:13 -0400

    Update the test.py file, tracking next steps

--- Test.py
+++ Test.py
@@ -1,6 +1,2 @@
-# I want to calculate the SSM using the pitches. I want to start from the high level, and calling unimplemented functions is ok. 
-
-# 1. Let's sign into Spotify and get the audio analysis for a song.
-
 # Currently: 
-# Create functions to Gaussian blur the SSM in the compute_harmonic_structure function.
+# Enhancing SSM. 
